00ad089ee9d28b6dfd4f99f381d1d67f18560115
Vincent Debergue <v.debergue@samsung.com>
2021-01-21 12:15:04 +0100

    resize and enhance image before display

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ set -o pipefail
 export ssh_user=${PI_USER:-pi}
 export ssh_host=${PI_HOST:-raspberrypi.local}
 export ssh_port=${PI_PORT:-22}
+FAST=${FAST:0}
 
 function rpi_exec() {
   ssh "$ssh_user"@"$ssh_host" -p "$ssh_port" $@
@@ -13,19 +14,23 @@ function rpi_exec() {
 
 root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Compile TS
-cd $root_dir/downloader
-npm install
-npm run dist
+if [[ "$FAST" == "0" ]]; then
+  cd $root_dir/downloader
+  npm install
+  npm run dist
+fi
 
 cd $root_dir
 
-rpi_exec << EOF
+if [[ "$FAST" == "0" ]]; then
+  rpi_exec << EOF
   sudo mkdir -p /opt/rasp-frame/tmp
   sudo mkdir -p /opt/rasp-frame/downloader
   sudo mkdir -p /opt/rasp-frame/imager
   sudo mkdir -p /opt/rasp-frame/IT8951
   sudo chown -R $ssh_user:$ssh_user /opt/rasp-frame
 EOF
+fi
 
 rsync -azP -e "ssh -p $ssh_port" $root_dir/downloader/dist "$ssh_user"@"$ssh_host":/opt/rasp-frame/downloader/
 rsync -azP -e "ssh -p $ssh_port" $root_dir/imager "$ssh_user"@"$ssh_host":/opt/rasp-frame
--- imager/main.py
+++ imager/main.py
@@ -1,8 +1,8 @@
 import argparse
 
-from PIL import Image, ImageDraw, ImageFont
-from IT8951 import constants
-from IT8951.display import AutoEPDDisplay
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+
+dims = (1872,1404)
 
 def main():
   parser = argparse.ArgumentParser(conflict_handler="resolve")
@@ -11,6 +11,8 @@ def main():
 
   print(f"Reading image from {args.image}")
 
+  from IT8951 import constants
+  from IT8951.display import AutoEPDDisplay
   display = AutoEPDDisplay(vcom=-1.55)
   print('VCOM set to', display.epd.get_vcom())
 
@@ -19,15 +21,57 @@ def main():
   display.clear()
 
   image = Image.open(args.image)
-  dims = (1872,1404)
-  image.thumbnail(dims)
-  paste_coords = [dims[i] - image.size[i] for i in (0,1)]  # align image with bottom of display
+  image = resize_image(image, dims)
+  image = enhance_image(image)
+  paste_coords = get_paste_coords(image)
   # image.show()
   print(f"{image}")
+  image = image.transpose(Image.FLIP_LEFT_RIGHT)
   display.frame_buf.paste(image, paste_coords)
   display.draw_full(constants.DisplayModes.GC16)
   print("done")
 
+def get_paste_coords(image):
+  # return [dims[i] - image.size[i] for i in (0,1)]  # align image with bottom of display
+  return [dims[i]//2  - image.size[i]//2 for i in (0,1)]  # align image with middle of display
+
+def resize_image(image, target_dims):
+  image = crop_to_aspect(image, dims[0], dims[1])
+  image.thumbnail(target_dims)
+  return image
+
+def enhance_image(image):
+  contrast = ImageEnhance.Contrast(image)
+  image = contrast.enhance(1.1)
+  sharpness = ImageEnhance.Sharpness(image)
+  image = sharpness.enhance(1.1)
+  return image
+
+def crop_to_aspect(image, aspect, divisor=1, alignx=0.5, aligny=0.5):
+    """Crops an image to a given aspect ratio.
+    Args:
+        aspect (float): The desired aspect ratio.
+        divisor (float): Optional divisor. Allows passing in (w, h) pair as the first two arguments.
+        alignx (float): Horizontal crop alignment from 0 (left) to 1 (right)
+        aligny (float): Vertical crop alignment from 0 (left) to 1 (right)
+    Returns:
+        Image: The cropped Image object.
+    """
+    if image.width / image.height > aspect / divisor:
+        newwidth = int(image.height * (aspect / divisor))
+        newheight = image.height
+    else:
+        newwidth = image.width
+        newheight = int(image.width / (aspect / divisor))
+    img = image.crop((alignx * (image.width - newwidth),
+                      aligny * (image.height - newheight),
+                      alignx * (image.width - newwidth) + newwidth,
+                      aligny * (image.height - newheight) + newheight))
+    return img
 
 if __name__ == "__main__":
-    main()
+  main()
+  # image = Image.open("tmp/image.jpg")
+  # image = enhance_image(image)
+  # image = resize_image(image, dims)
+  # image.show()
